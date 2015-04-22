6ab4f1dc24c1542e6e1a69a0dbb65b51c437c599
akinwaleagbaje <akinwaleagbaje@yahoo.com>
2015-04-22 16:49:42 +0100

    Added Descriptor class to decipher query descriptor

---
+++ src/org/greenpole/util/Descriptor.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.greenpole.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author Akinwale.Agbaje
+ * Splits descriptor into its multiple parts.
+ */
+public class Descriptor {
+    /**
+     * Breaks the descriptor into its individual search descriptions.
+     * descriptor is: clientCompany:none;shareUnit:none;numberOfShareholders:none;numberOfBondholders:none
+     * @param descriptor the search description
+     * @return a map of the individual search descriptions
+     */
+    public static Map<String, String> decipherDescriptor(String descriptor) {
+        Map<String, String> descriptorSplits = new HashMap<>();
+        String[] individualSplit = descriptor.split(";");
+        for (String singleDescriptor : individualSplit) {
+            String[] keyAndValue = singleDescriptor.split(":");
+            descriptorSplits.put(keyAndValue[0], keyAndValue[1]);
+        }
+        return descriptorSplits;
+    }
+}
