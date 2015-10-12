5d7ad502acab5ef679d313397869dd692a0f3a45
akinwaleagbaje <akinwaleagbaje@yahoo.com>
2015-10-12 13:33:36 +0100

    Optimised email client to allow for CC'ing email addresses

--- src/org/greenpole/util/email/EmailClient.java
+++ src/org/greenpole/util/email/EmailClient.java
@@ -7,6 +7,7 @@ package org.greenpole.util.email;
 
 import com.sun.mail.smtp.SMTPTransport;
 import java.text.MessageFormat;
+import java.util.List;
 import java.util.concurrent.Callable;
 import javax.mail.Authenticator;
 import javax.mail.Message;
@@ -37,6 +38,7 @@ public class EmailClient implements Runnable, Callable<String> {
     private final String to_person;
     private final String body_main;
     private final String template;
+    private final List<String> cc;
     
     /**
      * Initialises all necessary components before email is sent via thread.
@@ -55,12 +57,36 @@ public class EmailClient implements Runnable, Callable<String> {
         this.template = template;
         this.to_person = to_person;
         this.body_main = body_main;
+        this.cc = null;
+        initialiseProperties(from, password);
+    }
+
+    /**
+     * Initialises all necessary components before email is sent via thread.
+     * Used to send emails to multiple recipients.
+     * @param from the address the email will be sent from
+     * @param password the password of the from address (user address)
+     * @param to the address the email is going to
+     * @param subject the subject of the email
+     * @param to_person the name that should come under "dear ..." in the email template (not necessary in all templates)
+     * @param body_main the main body of the email that should be inserted into the template
+     * @param template the template which will serve as the email body, typically a html file
+     * @param cc email addresses to be attached to cc
+     */
+    public EmailClient(String from, String password, String to, String subject, String to_person, String body_main, String template, List<String> cc) {
+        this.from = from;
+        this.to = to;
+        this.subject = subject;
+        this.to_person = to_person;
+        this.body_main = body_main;
+        this.template = template;
+        this.cc = cc;
         initialiseProperties(from, password);
     }
     
     private void initialiseProperties(String user, String password) {
-        prop.setProperty(prop.getMailUser(), user);
-        prop.setProperty(prop.getMailPassword(), password);
+        prop.setProperty("mail.user", user);
+        prop.setProperty("mail.password", password);
         mailSession = Session.getInstance(prop, new SMTPAuthenticator());
         mailSession.setDebug(false);
         msg = new MimeMessage(mailSession);
@@ -82,6 +108,17 @@ public class EmailClient implements Runnable, Callable<String> {
             msg.setFrom(new InternetAddress(from));
             msg.setRecipient(Message.RecipientType.TO, new InternetAddress(to));
             msg.setSubject(subject);
+            
+            if (cc != null && !cc.isEmpty()) {
+                InternetAddress[] addys = new InternetAddress[cc.size()];
+                int index = 0;
+                for (String email : cc) {
+                    addys[index] = new InternetAddress(email);
+                    ++index;
+                }
+                msg.setRecipients(Message.RecipientType.CC, addys);
+            }
+            
             MessageFormat mf = new MessageFormat(template);
             String mail_content = mf.format(new Object[]{to_person,body_main});
             msg.setContent(mail_content, "text/html");
@@ -106,6 +143,17 @@ public class EmailClient implements Runnable, Callable<String> {
             msg.setFrom(new InternetAddress(from));
             msg.setRecipient(Message.RecipientType.TO, new InternetAddress(to));
             msg.setSubject(subject);
+            
+            if (cc != null && !cc.isEmpty()) {
+                InternetAddress[] addys = new InternetAddress[cc.size()];
+                int index = 0;
+                for (String email : cc) {
+                    addys[index] = new InternetAddress(email);
+                    ++index;
+                }
+                msg.setRecipients(Message.RecipientType.CC, addys);
+            }
+            
             MessageFormat mf = new MessageFormat(template);
             String mail_content = mf.format(new Object[]{to_person,body_main});
             msg.setContent(mail_content, "text/html");
