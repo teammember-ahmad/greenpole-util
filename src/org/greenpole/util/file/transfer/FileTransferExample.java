5abe1f1fb25f74444a21a3ba2bb7ef8545128491
emmanuel.idoko <emmanuel.idoko@APR-APPDEV.africaprudentialregistrars.com>
2017-01-03 17:22:24 +0100

    Implemented method for uploading file to ftp server

---
+++ src/org/greenpole/util/file/transfer/FileTransferExample.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.greenpole.util.file.transfer;
+
+/**
+ *
+ * @author emmanuel.idoko
+ */
+public class FileTransferExample {
+
+    public static void main(String[] args) throws Exception {
+        /*String servername = "192.168.10.150";
+        int port = 21;
+        String username = "emmanuel.idoko";
+        String password = "Desterity8080&$";*/
+        String localFileName = "C:\\Users\\emmanuel.idoko\\Documents\\TowersOfHanoiSolution.txt";
+        String hostDir = "/Tester";
+
+        FileTransfer ftpobj = new FileTransfer();
+        ftpobj.uploadFTPFile(localFileName, hostDir);
+    }
+}
