dece7f75f0c04912626a9331bb88468a3521de1a
Akinwale.Agbaje <Akinwale.Agbaje@APR-APPDEV01.africaprudentialregistrars.com>
2017-02-03 17:53:18 +0100

    Added new audit log for vendor applications

--- src/org/greenpole/util/audit/AuditLog.java
+++ src/org/greenpole/util/audit/AuditLog.java
@@ -34,7 +34,37 @@ public class AuditLog {
         org.slf4j.Logger logger = LoggerFactory.getLogger(AuditLog.class);
         boolean result = false;
         try {
-            result = gq.logUserActivity(userEmail, userIpAddress, functionPerformed, approvalRequired, designatedApprover, isAuthorisation, notificationCode, responseCode, responseDescription);
+            result = gq.logUserActivity(userEmail, userIpAddress, functionPerformed, approvalRequired, designatedApprover, isAuthorisation, 
+                    notificationCode, responseCode, responseDescription, null, "greenpole");
+        } catch (Exception ex) {
+            logger.info("error thrown while logging user activity for {}. See error log", userEmail);
+            logger.error("error thrown while logging user activity for " + userEmail, ex);
+        }
+        return result;
+    }
+    
+    /**
+     * Log user's activity.
+     * @param userEmail the user's email
+     * @param userIpAddress the user's ip address
+     * @param functionPerformed the user function performed
+     * @param approvalRequired if approval is required for this function
+     * @param designatedApprover the designated approver for this function (if any)
+     * @param isAuthorisation if the function performed is an authorisation
+     * @param notificationCode the notification code
+     * @param responseCode the server's response code
+     * @param responseDescription the server's response description
+     * @param referenceId the reference id
+     * @return true, if activity is logged
+     */
+    public static boolean logErsUserActivity(String userEmail, String userIpAddress, String functionPerformed, boolean approvalRequired, String designatedApprover, 
+            boolean isAuthorisation, String notificationCode, int responseCode, String responseDescription, String referenceId) {
+        GeneralComponentQuery gq = ComponentQueryFactory.getGeneralComponentQuery();
+        org.slf4j.Logger logger = LoggerFactory.getLogger(AuditLog.class);
+        boolean result = false;
+        try {
+            result = gq.logUserActivity(userEmail, userIpAddress, functionPerformed, approvalRequired, designatedApprover, isAuthorisation, 
+                    notificationCode, responseCode, responseDescription, referenceId, "ers");
         } catch (Exception ex) {
             logger.info("error thrown while logging user activity for {}. See error log", userEmail);
             logger.error("error thrown while logging user activity for " + userEmail, ex);
