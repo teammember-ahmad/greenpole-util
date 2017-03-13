9d67167bbddea5b3ff8909d93de40c45fb255299
Akinwale.Agbaje <Akinwale.Agbaje@APR-APPDEV01.africaprudentialregistrars.com>
2017-03-13 15:19:47 +0100

    AGM Util changes

---
+++ src/org/greenpole/util/agm/AgmProperties.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.greenpole.util.agm;
+
+/**
+ *
+ * @author Akinwale.Agbaje
+ */
+public class AgmProperties {
+    
+}
