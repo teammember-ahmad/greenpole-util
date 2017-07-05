c152cfc8ed12bc7c14e7c95b214d8bab53146af4
Akinwale.Agbaje <Akinwale.Agbaje@APR-APPDEV01.africaprudentialregistrars.com>
2017-07-05 13:19:24 +0100

    INC-981: Test finalised for retrieving xml map values into object

--- src/org/greenpole/util/rest/RestCheckTester.java
+++ src/org/greenpole/util/rest/RestCheckTester.java
@@ -10,6 +10,12 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import org.greenpole.entity.model.profiler.UserProfile;
+import org.greenpole.entity.model.profiler.ViewGroup;
 import org.greenpole.entity.model.user.UserAccess;
 import org.greenpole.entity.response.Response;
 
@@ -18,29 +24,68 @@ import org.greenpole.entity.response.Response;
  * @author Akinwale.Agbaje
  */
 public class RestCheckTester {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, JAXBException {
         ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
                 .configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
         
         RestCheck check = new RestCheck();
         System.out.println("started running...");
-        String rtn = check.doPost();
+        Response rtn = check.doPost();
+        
+        JAXBContext context = JAXBContext.newInstance(Response.class);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        marshaller.marshal(rtn, System.out);
+        
+        Map<String, ViewGroup> views = ((UserProfile)rtn.getRestBody().get(0)).getViews();
+        for (Map.Entry pairs : views.entrySet()) {
+            String key = (String) pairs.getKey();
+            ViewGroup group = (ViewGroup) pairs.getValue();
+            
+            System.out.println("key: " + key);
+            System.out.println("group: " + group.getGroupName());
+        }
+        
         System.out.println(rtn);
         System.out.println("##################################################");
         System.out.println("##################################################");
-        Response resp = mapper.readValue(rtn, Response.class);
+        
+        System.exit(0);
+        Response resp = mapper.readValue("", Response.class);
         //Response resp2 = mapper.convertValue(rtn, Response.class);
         
         
         System.out.println(mapper.writeValueAsString(resp));
+        System.out.println("retn: " + resp.getRetn());
         
         /*List<UserAccess> ulist = (List<UserAccess>) resp.getRestBody();
         for (UserAccess a : ulist) {
             System.out.println("email: " + a.getEmail());
         }*/
         
-        /*String json = mapper.writeValueAsString(resp.getRestBody());
-        List<UserAccess> ulist = mapper.readValue(json, new TypeReference<List<UserAccess>>(){});
+        String json = mapper.writeValueAsString(resp.getRestBody());
+        System.out.println("############################################################");
+        System.out.println("############################################################");
+        System.out.println("############################################################");
+        System.out.println("############################################################");
+        System.out.println(json);
+        List<UserProfile> uprofile_list = mapper.readValue(json, new TypeReference<List<UserProfile>>(){});
+        System.out.println("############################################################");
+        System.out.println("############################################################");
+        System.out.println("############################################################");
+        System.out.println("############################################################");
+        System.out.println(mapper.writeValueAsString(uprofile_list));
+        
+        System.out.println("############################################################");
+        System.out.println("############################################################");
+        System.out.println("########################-- LIST OF USER PROFILE --####################################");
+        for (Object up : uprofile_list) {
+            String obj_json = mapper.writeValueAsString(up);
+            System.out.println(obj_json);
+            UserProfile profile = mapper.readValue(obj_json, UserProfile.class);
+        }
+        
+        /*List<UserAccess> ulist = mapper.readValue(json, new TypeReference<List<UserAccess>>(){});
         for (UserAccess a : ulist) {
             System.out.println("email: " + a.getEmail());
         }*/
